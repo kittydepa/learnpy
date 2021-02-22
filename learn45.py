0d75a29aaaa80ed8d400bc5ec0c91dee31ad85fa
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-02-22 14:17:28 +0100

    Adding time.sleep(), and some ** to text

--- learn45.py
+++ learn45.py
@@ -1,8 +1,9 @@
 from sys import exit
-from random import randint
+#from random import randint
 from textwrap import dedent
 import typing
 from charms import Eval  # Importing a function from another file
+import time
 
 class Room:
 
@@ -31,9 +32,10 @@ class Game:
 # The mini-game. ALl characters will end up here regardless.
 class MiniGame:
 
-    def enter(self):
+    def enter(self) -> str:
         print(" ")
         print("--------------------------------Game of the Gods--------------------------------")
+        time.sleep(2)
         print(dedent("""
               You enter a special room where three tiles lay before you.
               Stepping on one of the tiles will reward you with a powerup.
@@ -43,15 +45,21 @@ class MiniGame:
         tile_choice = input("> ")
 
         if tile_choice == "1":
+            time.sleep(1)
             print("That was not the correct tile. You lose half of your health.")
+            time.sleep(1)
             return 'death'
 
         elif tile_choice == "2":
+            time.sleep(1)
             print("That was not the correct tile. A giant Oger appears and kills you with a single blow.")
+            time.sleep(1)
             return 'death'
 
         elif tile_choice == "3":
+            time.sleep(1)
             print("You got the powerup! You are rewarded with... nothing!")
+            time.sleep(1)
             return 'cave'
         
         else:
@@ -86,20 +94,20 @@ class Characters(Room):
 
             if selection.lower() == "john" or selection.lower() == "linda":
                 print("Good choice. This character is a mighty fighter.")
+                time.sleep(1)
                 print("You must find and rescue Mary before proceeding to the Caves.")
-                print(" ----------------------------------------------------------------")
                 return 'mary'
 
             elif selection.lower() == "mark" or selection.lower() == "lucy":
                 print("Good choice. This character dabbles a bit in magic and is very swift.")
+                time.sleep(1)
                 print("You must help Grandma Margaret find some special herbs before proceeding to the Caves.")
-                print(" ----------------------------------------------------------------")
                 return 'grandma'
 
             elif selection.lower() == "kevin" or selection.lower() == "joey":
                 print("Good choice. This character is quite skillfull and strong.")
-                print("You must find and rescue Mary before proceeding to the Caves.")
-                print(" ----------------------------------------------------------------")
+                time.sleep(1)
+                print("You must fight through monsters in the cave.")
                 return 'fight'
 
             else:
@@ -118,8 +126,10 @@ class Finished(Room):
 
     def enter(self):
         print(" ")
+        time.sleep(1)
         print("------------------------------Victory!--------------------------------")
         print("You made it out in one piece!!! Good job.")
+        time.sleep(1)
         x = Eval()
         return x
 
@@ -131,21 +141,24 @@ class Cave(Room):
             print("---------------------------Cave of the Spider King---------------------------")
             print(dedent("""
                   You made it to the final level! The Cave. Time to defeat the
-                  Spider King. You have two options: either 1 or 2
+                  Spider King. You have two options: either *1* or *2*
                   """))
             
             choice = input("> ")
 
             if choice == "1":
+                time.sleep(1)
                 print("Damn, you made it so far but did not make it in the end.")
                 print("You died.")
                 return 'death'
 
             elif choice == "2": # something:
+                time.sleep(1)
                 print("Spider King = defeated!")
                 return 'finished'
 
             else:
+                time.sleep(1)
                 print("Huh? What's that mean? Try again.")
                 return 'cave'
 
@@ -155,6 +168,7 @@ class RescueMary(MiniGame):
     def enter(self) -> str:
         print(" ")
         print("--------------------------------Rescue Mary--------------------------------")
+        time.sleep(1)
         print(dedent("""
               You enter a room and see Mary being held down by a mob of ghouls,
               with a little bit of her life draining each minute. You can either
@@ -162,18 +176,21 @@ class RescueMary(MiniGame):
               to kill them all at once. What do you do?"""))
         action = input("> ")
 
-        if "one" in action.lower() or "attack" in action.lower():
+        if "one" in action.lower().strip() or "attack" in action.lower().strip():
+            time.sleep(1)
             print("You fight them, and win. Woohooo!")
             minigame = super(RescueMary, self)
             x = minigame.enter()
             return x
 
-        elif "charge" in action.lower():
+        elif "charge" in action.lower().strip():
+            time.sleep(1)
             print("Damn, well that did not go as expected. They clobbered you.")
             return 'death'
 
         else:
             print("I'm sorry, I don't understand. Try again.")
+            time.sleep(1)
             return 'mary'
 
 
@@ -182,18 +199,20 @@ class HelpGrandma(MiniGame):
     def enter(self) -> str:  # 1. Apply to all other enter functions. 
         print(" ")
         print("--------------------------------Help Grandma--------------------------------")
+        time.sleep(1)
         print(dedent("""
               You see the special herb that Grandma Margaret needs for
               healing potions. However, the herb is surronded by hundreds
               of tiny spiders. You have two choices. You can try to use your
-              magic to freeze the spiders for a split second, or you can try
-              to become invisible and invulnerable for a few moments to try to
+              magic to *freeze* the spiders for a split second, or you can try
+              to become *invisible* and invulnerable for a few moments to try to
               retreive the herb. Which one do you choose?
               """))
 
         action = input("> ")
 
         if "freeze" == action.lower().strip(): # Calling .strip on the result of the lower function  # Chained function call
+            time.sleep(1)
             print(dedent("""
                   Well... that didn't go as expected. The spiders did freeze, but
                   not for long enough. You made your way to the herb but just as you
@@ -202,7 +221,8 @@ class HelpGrandma(MiniGame):
                   """))
             return 'death'
 
-        elif "invisible" in action.lower():
+        elif "invisible" in action.lower().strip():
+            time.sleep(1)
             print(dedent("""
                   You conjure up enough mana to be able to make yourself invisible.
                   You treaded cautiously as you were not sure how long you could stay
@@ -217,6 +237,7 @@ class HelpGrandma(MiniGame):
 
         else:
             print("What? ... Try again.")
+            time.sleep(1)
             return "grandma"
         
        # raise Exception("Function must return as the string.") # 
@@ -227,6 +248,7 @@ class PreCave(MiniGame):
     def enter(self) -> str:
         print(" ")
         print("--------------------------------Cave 1--------------------------------")
+        time.sleep(1)
         print(dedent("""
               You are in a cave and need to battle your way out. Inside
               the cave where are dozens of demonic archers, all pointing
@@ -241,6 +263,7 @@ class PreCave(MiniGame):
         choice = input("> ")
 
         if "knives" == choice.lower().strip():
+            time.sleep(1)
             print(dedent("""
                   You rush towards the epicenter of all of the chaos. You
                   jump in the air, surrounded by enemies, and attempt to 
@@ -250,13 +273,15 @@ class PreCave(MiniGame):
             x = minigame.enter()
             return x
 
-        elif "strike" == choice.lower().strip(): 
+        elif "strike" == choice.lower().strip():
+            time.sleep(1) 
             print("Omg that was the wrong choice. Very bad. You died.")
             return 'death'
 
         else:
             print("... the fuck does that mean? Try again")
-        return 'fight'
+            time.sleep(1)
+            return 'fight'
 
 # Dict with all events, and the code for how to go from 1 room to the next
 class Events:
