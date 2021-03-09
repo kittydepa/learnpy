c3764b0327afde000be5933702fad2477e4144cc
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-09 17:41:53 +0100

    loops

---
+++ loops.py
@@ -0,0 +1,16 @@
+## Part 1. Loops that print even numbers from 1-10
+
+# a. For loop
+
+for i in range(0, 10):
+    if i % 2 == 0:
+        print(i)
+
+# b. While loop
+
+
+# c. Recursive loop
+
+
+
+## Part 2. Loops that print numbers divisible by 3, from 1-30
---
+++ twelvedays.py
@@ -0,0 +1,23 @@
+# Setting the stage:
+
+def intro():
+    days = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eigth", "ninth", "tenth", "eleventh", "twelfth"]
+    for i in days[0:]:
+        print(f"On the {i} day of Christmas my true love gave to me: ")
+
+def lines():
+    gifts = ["g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "g10", "g11", "g12"]
+    for j in gifts[0:]:
+
+
+
+# Where the real magic happens!
+
+def song():
+    intro()
+    lines()
+
+
+
+
+song()
