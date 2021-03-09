5741fc8a27d2a688add2c5096d404704a430284a
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-09 20:53:04 +0100

    up to 30 for and while loop

--- loops.py
+++ loops.py
@@ -12,7 +12,7 @@
 
 # i = 0
 
-# while i  < 10:
+# while i  < 10: # This is true
 #     i += 2
 #     print(i)
 
@@ -21,5 +21,26 @@
 # c. Recursive loop
 
 
+# d. Using 'break'
 
-## Part 2. Loops that print numbers divisible by 3, from 1-30
+
+
+
+### Part 2. Loops that print numbers divisible by 3, from 1-30
+
+# # a. For loop
+# for i in range(1, 31):
+#     if i % 3 == 0:
+#         print(i)
+
+# # b. While loop
+# i = 0
+
+# while i < 30:
+#     i += 3
+#     print(i)
+
+# c. Recursive loop
+
+
+# d. Using break
