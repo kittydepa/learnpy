051ec0f9ac85e70b8b7514d9e65956e91e4fd2cc
Kitty Depa <katherinedepa@gmail.com>
2021-03-15 16:02:46 +0100

    Make a list of random numbers to work with

---
+++ rust_ex1_python.py
@@ -0,0 +1,15 @@
+# Given a list of integers, use a vector and return the mean (the average value), 
+# median (when sorted, the value in the middle position), and mode 
+# (the value that occurs most often; a hash map will be helpful here) of the list.
+
+
+# # Step 1. make a random list of numbers to work with
+# import random 
+
+# list = []
+# for i in range(0, 5):
+#     n = random.randint(1, 10)
+#     list.append(n)
+# print(list) # Manually enter the output of this into list, so we are working with the same set of no.
+
+list = [9, 7, 7, 9, 4]
