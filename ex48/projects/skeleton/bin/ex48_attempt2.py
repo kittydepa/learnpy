39c9cf6806c9227236c08129f45eb49a9b497cec
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-29 13:40:27 +0200

    Added NOT MY ORIGINAL comments

--- ex48/projects/skeleton/bin/ex48_attempt2.py
+++ ex48/projects/skeleton/bin/ex48_attempt2.py
@@ -27,26 +27,38 @@ lexicon = {
 def scan(sentence):
     sentence = sentence.split()
     result = []
+    """ sentence got its value from scan('input').
+        Then it splits it up in parts and will be values 
+        for word in the loop.
+        For each loop word compare its value(part) 
+        to the content in lexicon."""
     for word in sentence:
         wordtype = lexicon.get(word, 'error')
         pair = (wordtype, word)
 
+        """If there is a match it will append the wordtype + word to result. """
         if word in lexicon:
             result.append(pair)
 
+            """ Then it look if it is a number(str). 
+            If so it convert it with function convert_int to a integer. 
+            Then pair adds 'number + the integer to a."""
         elif word not in lexicon:
             number = convert_number(word)
             if number:
                 pair = ('number', number)
-
+                
+                """If neither of the two previous condition is met, 
+                it sets pair to wordtype with its default value (error)"""
             elif word not in lexicon and not number:
                 pair = (wordtype, word)
 
-            result.append(pair)
+            result.append(pair) # This append from conditions #2 and #3
 
-    print (result)  # printing this to see the output
-    return result
-
+    print (result)  # For humans
+    return result # For python
+    
+# This function is used in condition #2
 def convert_number(s):
     try:
         return int(s)
