127aa830b6d5ccf3c68eca47f0a3d8fe6dd6529d
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-29 19:22:00 +0200

    More tests

--- study_pytest/test_sample1.py
+++ study_pytest/test_sample1.py
@@ -3,10 +3,10 @@ import pytest
 def test_file1_method1():
     x = 5
     y = 6
-    assert x + 1 == y, "test failed"
+    assert x + 2 == y, "test failed because x = " + str(x) + " y = " + str(y) # This prints some more info about the failure IF we get a failure. PyTest also gives some good details on its own though?
     assert x == y, "test failed"
 
-def test_file1_method1():
+def test_file1_method2():
     x = 5
     y = 6
-    assert x + 2 == y, "test failed"
+    assert x + 1 == y, "test failed"
