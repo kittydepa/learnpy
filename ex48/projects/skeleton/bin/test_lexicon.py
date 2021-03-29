bef4a1e260bd557505903b41e2972775f37c2f51
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-29 13:53:59 +0200

    Changing test script to match other ex48 script from gut

--- ex48/projects/skeleton/bin/test_lexicon.py
+++ ex48/projects/skeleton/bin/test_lexicon.py
@@ -5,60 +5,53 @@
 import pytest
 
 from ex48_attempt2 import scan
-# from ex48_attempt2 import convert_number
-# from ex48_attempt2 import scan
-# from ex48 import lexicon
+from ex48_attempt2 import convert_number
+from ex48_attempt2 import lexicon
+
 
 
 def test_directions():
-    
+
     assert scan("north") == [('direction', 'north')]
     result = scan("north south east")
     assert result == [('direction', 'north'),
                       ('direction', 'south'),
                       ('direction', 'east')]
 
-# def test_verbs():
-#     assert lexicon.scan("go") == [('verb', 'go')]
-#     result = lexicon.scan("go kill eat")
-#     assert result == [('verb', 'go'),
-#                       ('verb', 'kill'),
-#                       ('verb', 'eat')]
+def test_verbs():
+
+    assert scan("go") == [('verb', 'go')]
+    result = scan("go kill eat")
+    assert result == [('verb', 'go'),
+                      ('verb', 'kill'),
+                      ('verb', 'eat')]
 
 
-# def test_stops():
-#     assert lexicon.scan("the") == [('stop', 'the')]
-#     result = lexicon.scan("the in of")
-#     assert result == [('stop', 'the'),
-#                       ('stop', 'in'),
-#                       ('stop', 'of')]
+def test_stops():
+    assert scan("the") == [('stop', 'the')]
+    result = scan("the in of")
+    assert result == [('stop', 'the'),
+                      ('stop', 'in'),
+                      ('stop', 'of')]
 
 
-# def test_nouns():
-#     assert lexicon.scan("bear") == [('noun', 'bear')]
-#     result = lexicon.scan("bear princess")
-#     assert result == [('noun', 'bear'),
-#                       ('noun', 'princess')]
+def test_nouns():
+    assert scan("bear") == [('noun', 'bear')]
+    result = scan("bear princess")
+    assert result == [('noun', 'bear'),
+                      ('noun', 'princess')]
 
 
-# def test_numbers():
-#     assert lexicon.scan("1234") == [('number', 1234)]
-#     result = lexicon.scan("3 91234")
-#     assert result == [('number', 3),
-#                       ('number', '91234')]
+def test_numbers():
+    assert convert_number("1234") == [('number', 1234)]
+    result = convert_number("3 91234")
+    assert result == [('number', 3),
+                      ('number', '91234')]
 
 
-
-# def test_errors():
-#     assert lexicon.scan("ASDFADFASDF") == [('error', 'ASDFADFASDF')]
-#     result = lexicon.scan("bear IAS princess")
-#     assert result == [('noun', 'bear'),
-#                       ('error', 'IAS'),
-#                       ('noun',=======
-# def test_errors():
-#     assert lexicon.scan("ASDFADFASDF") == [('error', 'ASDFADFASDF')]
-#     result = lexicon.scan("bear IAS princess")
-#     assert result == [('noun', 'bear'),
-#                       ('error', 'IAS'),
-#                       ('noun', 'princess')]
-
+def test_errors():
+    assert scan("ASDFADFASDF") == [('error', 'ASDFADFASDF')]
+    result = scan("bear IAS princess")
+    assert result == [('noun', 'bear'),
+                      ('error', 'IAS'),
+                      ('noun', 'princess')]
