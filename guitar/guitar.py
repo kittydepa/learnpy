6a611c3f898c10c0e6a6eb80d8d95c2d742da79a
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-30 14:19:40 +0200

    Adding whole notes

--- guitar/guitar.py
+++ guitar/guitar.py
@@ -1,1 +1,7 @@
 # https://betterprogramming.pub/how-to-learn-guitar-with-python-978a1896a47?fbclid=IwAR1dMg6yxfgASXhz-pyyyfwW_cbOJC3FfXzRnbRs1F_ElYduxjW-QtzRY_I&gi=53f4904528e7
+
+whole_notes = ['C', 'C#', 'D', 'D#', 'E', 'F',
+               'F#', 'G', 'G#', 'A', 'A#', 'B'] * 2
+
+
+print(whole_notes)
