493bdc4d53e9134d7532c13054f303005561dbd3
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-30 14:21:24 +0200

    Comments

--- guitar/guitar.py
+++ guitar/guitar.py
@@ -1,7 +1,10 @@
 # https://betterprogramming.pub/how-to-learn-guitar-with-python-978a1896a47?fbclid=IwAR1dMg6yxfgASXhz-pyyyfwW_cbOJC3FfXzRnbRs1F_ElYduxjW-QtzRY_I&gi=53f4904528e7
 
+# Using the intervals of a given scale as indexes
 whole_notes = ['C', 'C#', 'D', 'D#', 'E', 'F',
                'F#', 'G', 'G#', 'A', 'A#', 'B'] * 2
 
 
 print(whole_notes)
+
+# Plotting these notes, based on where they are on an actual guitar
