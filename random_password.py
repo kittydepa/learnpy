1da9729373db945e38dcc19916062920f5a4bfa3
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-31 12:45:25 +0200

    Password generator

--- random_password.py
+++ random_password.py
@@ -7,7 +7,6 @@
 2 punctuation signs such as !, ?, ", #, etc."
 '''
 # Will use ASCII for this https://www.101computing.net/wp/wp-content/uploads/ASCII-Table.pdf
-
 import random
 
 uppercaseLetter1 = chr(random.randint(65, 90))
@@ -22,6 +21,20 @@ digit2 = chr(random.randint(48, 57))
 punctuation1 = chr(random.randint(33, 39))
 punctuation2 = chr(random.randint(33, 39))
 
+#print(password)
+
+# Now we need to shuffle, make a function that shuffles
+def shuffle(x):
+    tempList = list(x)
+    random.shuffle(tempList)
+    return ''.join(tempList) # Why do we need this part/why is it written like this?
+
+
+# The 'formula' for what makes up a password
 password = (uppercaseLetter1 + lowercaseLetter1 + digit1 + punctuation1 + uppercaseLetter2 + lowercaseLetter2 + digit2 + punctuation2)
 
+# Appling the shuffle function to our password formula
+password = shuffle(password)
+
+# Printing the output
 print(password)
