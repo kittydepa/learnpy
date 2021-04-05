d21223a87dd1180a80996a8084be0394d1bed3fa
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-05 12:10:21 +0200

    Ex 51 changes

--- projects/ex50/bin/app.py
+++ projects/ex50/bin/app.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from flask import render_template # this function knows how to read html files
-from flask import request
 
 app = Flask(__name__, template_folder = r'C:\Users\Kitty\Desktop\learnpy\projects\gothonweb\templates')
 
-@app.route("/hello") # this refers to 'index' mapping, flask will use 'def index'
+@app.route("/") # this refers to 'index' mapping, flask will use 'def index'
 def index():
-    name = request.args.get('name', 'Nobody')
-    
-    if name:
-        greeting = f"Hello {name}"
-    else:
-        greeting = "Hello World"
-    
+    greeting = "Hello World"
     return render_template("index.html", greeting=greeting)
 
 if __name__ == "__main__":
