13e7a818e73463a9cb6d61ef0fec24b2e05a5bef
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-05 12:35:29 +0200

    ex 51 progress

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -4,16 +4,18 @@ from flask import request
 
 app = Flask(__name__, template_folder = r'C:\Users\Kitty\Desktop\learnpy\projects\gothonweb\templates')
 
-@app.route("/hello") # this refers to 'index' mapping, flask will use 'def index'
+@app.route("/hello", methods = ['POST', 'GET']) # this refers to 'index' mapping, flask will use 'def index'
 def index():
-    name = request.args.get('name', 'Nobody')
-    
-    if name:
-        greeting = f"Hello {name}"
+    greeting = "Hello World"
+
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet}, {name}"
+        return render_template("index.html", greeting = greeting)
     else:
-        greeting = "Hello World"
-    
-    return render_template("index.html", greeting=greeting)
+        return render_template("hello_form.html")
+   
 
 if __name__ == "__main__":
     app.run()
