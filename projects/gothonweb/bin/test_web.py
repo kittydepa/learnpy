244fc031ecb3308a82560120d0d8799315269106
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-06 13:08:58 +0200

    Added a comment

--- projects/gothonweb/bin/test_web.py
+++ projects/gothonweb/bin/test_web.py
@@ -10,7 +10,7 @@ def test_index():
 
     rv = webz.get('/hello', follow_redirects = True)
     assert rv.status_code == 200
-    assert b"Fill Out This Form" in rv.data
+    assert b"Fill Out This Form" in rv.data # Need b because bytes yo, not str
 
     data = {'name': 'Kitty', 'greet': 'Hej'}
     rv = webz.post('/hello', follow_redirects = True, data = data)
