37caf7aecb4caf8ebab4b1a84e12e315b345eaad
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-06 13:04:00 +0200

    Ex 51 PyTest Success

--- projects/gothonweb/bin/test_web.py
+++ projects/gothonweb/bin/test_web.py
@@ -1,2 +1,18 @@
-import pytest 
-#import web
+import pytest
+from web import app
+
+app.config['TESTING'] = True
+webz = app.test_client()
+
+def test_index():
+    rv = webz.get('/', follow_redirects = True)
+    assert rv.status_code == 404
+
+    rv = webz.get('/hello', follow_redirects = True)
+    assert rv.status_code == 200
+    assert b"Fill Out This Form" in rv.data
+
+    data = {'name': 'Kitty', 'greet': 'Hej'}
+    rv = webz.post('/hello', follow_redirects = True, data = data)
+    assert b"Kitty" in rv.data
+    assert b"Hej" in rv.data
