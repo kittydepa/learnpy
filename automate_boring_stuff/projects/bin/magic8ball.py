0b4e7811adf23582c2178d1bbf07ec8abd56b50c
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-07 14:54:52 +0200

    Added comment

--- automate_boring_stuff/projects/bin/magic8ball.py
+++ automate_boring_stuff/projects/bin/magic8ball.py
@@ -11,3 +11,6 @@ messages = ['It is certain',
     'Very doubtful']
 
 print(messages[random.randint(0, len(messages) - 1)])
+# The line above means: from the list, 'messages', give me the [] indexed value,
+# of a random item from the list, anywhere between the first, and 
+# total length (minus one cuz that is how you count in Python) of the entire list
