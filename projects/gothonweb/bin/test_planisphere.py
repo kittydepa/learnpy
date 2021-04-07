576a6afc803c41702251eec44f37107490d5f94c
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-07 14:29:57 +0200

    Adding to the test doc. Need to edit later

--- projects/gothonweb/bin/test_planisphere.py
+++ projects/gothonweb/bin/test_planisphere.py
@@ -30,3 +30,11 @@ def test_map():
     assert start.go('west') == west
     assert start.go('west').go('east') == start
     assert start.go('down').go('up') == start
+
+def test_gothon_game_map():
+    start_room = load_room(START)
+    assert start_room.go('shoot!') == generic_death
+    assert start_room.go('dodge!') == generic_death
+
+    room = start_room.go('tell a joke')
+    assert room == laser_weapon_armory
