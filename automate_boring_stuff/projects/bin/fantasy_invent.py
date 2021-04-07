69ac74efb75205e20563244ca9f06aeef1a86a22
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-07 19:18:03 +0200

    Step 2  attempt - broke Step 1 somehow

--- automate_boring_stuff/projects/bin/fantasy_invent.py
+++ automate_boring_stuff/projects/bin/fantasy_invent.py
@@ -6,10 +6,10 @@
 
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
-def displayInventory(inventory):
+def displayInventory(stuffs):
     print("Inventory:")
     item_total = 0
-    for k, v in inventory.items():                          # For dict k(key), in v(value) of the itens in the given dict,
+    for k, v in stuffs.items():                          # For dict k(key), in v(value) of the itens in the given dict,
         print(str(v) + ' ' + k)                             # Print it as a string, so it will be value ' ' key
         item_total += v                                     # Add this to the item total
     print("Total number of items: " + str(item_total))      # Print the total number of items, as a string :)
@@ -21,13 +21,16 @@ displayInventory(stuff)
 
 
 ## Step 2 - Make a function that will add items to the inventory 
+# List to dictionary function
 
-# def addToInventory(inventory, addedItems):
-#     inventory = 1
+def addToInventory(inventory, addedItems):
+    print("Inventory:")
+    for i in addedItems:
+        inventory[i] = addedItems.count(i)
     
 
 
-# inv = {'gold coin': 42, 'rope': 1}
-# dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-# inv = addToInventory(inv, dragonLoot)
-# displayInventory(inv)
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inven = addToInventory(inv, dragonLoot)
+displayInventory(inven)
