890249126a4a438a8ef755b27f17b93508e656bb
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-08 13:55:08 +0200

    Password locker complete

--- automate_boring_stuff/projects/bin/pw.py
+++ automate_boring_stuff/projects/bin/pw.py
@@ -1,14 +1,25 @@
 ## Password Locker Project- from 'Automate the Boring Stuff with Python'
-# Will store account names and their password - insecure
+## Will store account names and their password - insecure
 
+# Step 1 - Store passwords in a data structure, like a dict
 passwords = {'email': 'emailpassword123',
              'blog': 'blogpassword123',
              'facebook': 'facebookpassword123'}
 
 # Step 2 - Handling the Command Line Arguments
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()
 
-account = sys.argv[1]  # first commane line arg is the account name
+account = sys.argv[1]  # first commane line arg is the account name. 
+
+if account in passwords: # Looks in the pass dictuionare for the account name
+    pyperclip.copy(passwords[account])
+    print('Passwords for ' + account + ' copied to clipboard.') # if it's there, we get the password
+else:
+    print('There is no account named' + account) # if not, then told it does not exist in the passwords dict
+
+
+# To run this program, go to a command prompt and to where the file is stored
+# Then enter: 'python pw.py DICTKEY' to get that dict'keys' password
