b7a9ed8c6bcf881a43ed616ca6152fb8f2571738
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-09 19:44:58 +0200

    Initial commit- Renaming Dates Assignment

---
+++ automate_boring_stuff/projects/bin/renameDates.py
@@ -0,0 +1,6 @@
+'''From ch 9. in Automate the Boring Stuff
+Will make a script that will rename files that have a US-style
+date to European-style date. 
+'''
+
+# Step 1: Create a Regex for American-Style Dates
