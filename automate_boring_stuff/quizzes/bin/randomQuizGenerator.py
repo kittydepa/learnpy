f5dddf7516e3a8a861185db0abaabf369133d53a
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-09 13:55:16 +0200

    Step 2

---
+++ automate_boring_stuff/quizzes/bin/randomQuizGenerator.py
@@ -0,0 +1,44 @@
+'''
+Random US State-Capitol Quiz generator from 'Automate the Boring Stuff' pg 206
+    - Will create 35 different quizzes (questions presented in a different order)
+    - Muliple choice
+    - With answer key
+'''
+
+import random
+
+# The quiz data. Keys are states, and values are their capitals
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+            'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+            'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+            'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield',
+            'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka',
+            'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta',
+            'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing',
+            'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City',
+            'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 
+            'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe',
+            'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck',
+            'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 
+            'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia',
+            'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 
+            'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond',
+            'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+
+# Generate 35 different quiz files
+for quizNum in range(35): # Go through 35 times, aka the total no. of quizzes we want to have
+    # Create the quiz and answer key files.
+    quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w') # name of the quizzes will end with a number, that's why it is %s, to indicate which version of the quiz it is
+    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+
+    # Write out the header for the quiz.
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+
+    # Shuffle the order of the states.
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+    # TO DO: Loop through all 50 states, making a question for each state.
