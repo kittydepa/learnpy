ef50a2bc6457fd61bcf46c1a8b94d7e7c2655bed
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-12 16:59:16 +0200

    Another example

--- automate_boring_stuff/projects/bin/time.py
+++ automate_boring_stuff/projects/bin/time.py
@@ -16,16 +16,27 @@ You can do this by calling time.time() before and after a block of code
 
 # -------------------------------------------------
 
-import time
-def calcProd():
-    # calculate tje produce of the first 100, 000 numbers
-    product = 1
-    for i in range (1, 100000):
-        product = product * i
-    return product
+# import time
+# def calcProd():
+#     # calculate tje produce of the first 100, 000 numbers
+#     product = 1
+#     for i in range (1, 100000):
+#         product = product * i
+#     return product
 
-startTime = time.time()
-prod = calcProd()
-endTime = time.time()
-print('The result is %s digits long.' % (len(str(prod))))
-print('Took %s seconds to calculate.' % (endTime - startTime))
+# startTime = time.time()
+# prod = calcProd()
+# endTime = time.time()
+# print('The result is %s digits long.' % (len(str(prod))))
+# print('Took %s seconds to calculate.' % (endTime - startTime))
+
+# -------------------------------------------------
+
+# import time
+# for i in range(3):
+#     print('Tick')
+#     time.sleep(1)
+#     print('Tock')
+#     time.sleep(1)
+
+# time.sleep(5)
