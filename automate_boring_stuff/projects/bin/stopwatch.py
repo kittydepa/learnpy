eafcd8d29d5f008c4cdb6dbed35f650ea8232ce6
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-12 21:02:18 +0200

    Started - ch 15 Stopwatch project

---
+++ automate_boring_stuff/projects/bin/stopwatch.py
@@ -0,0 +1,7 @@
+"""
+From ch 15 of 'Automate the Boring Stuff'
+Super Stopwatch Project: Will track amount of time has gone by
+between presses of the ENTER key, with each key press = 1 'lap' on the timer.
+Will print the lap #, the total time, and lap time.
+"""
+
