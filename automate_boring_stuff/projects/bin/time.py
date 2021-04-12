5a96be82a6b8ceb605036dca0c563602f9ce53e2
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-12 16:16:45 +0200

    Ch 15 - time and scheduling notes + practice

---
+++ automate_boring_stuff/projects/bin/time.py
@@ -0,0 +1,11 @@
+# Ch 15 in 'Automate the boring stuff'
+
+import time
+x = time.time() 
+
+print(x) 
+'''
+time.time() will print out how many seconds have passed since the
+Unix epoch (a common time reference in programming), which is the
+date: 12 AM, Jan 1, 1970.
+'''
