33609c13d3033c845bf51e4febd898887d90bb69
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-04-13 10:22:36 +0200

    Ch 15 Stopwatch project

--- automate_boring_stuff/projects/bin/stopwatch.py
+++ automate_boring_stuff/projects/bin/stopwatch.py
@@ -5,3 +5,26 @@ between presses of the ENTER key, with each key press = 1 'lap' on the timer.
 Will print the lap #, the total time, and lap time.
 """
 
+import time
+
+# Display program's instructions
+print('Press ENTER to being. Afterwards, press ENTER to "click" the stopwatch. \n Press Ctrl-C to quit.')
+input()                # pressing Enter to begin
+print('Started.')
+
+startTime = time.time()  # get the first lap's start time
+lastTime = startTime
+lapNum = 1
+
+# Start tracking the lap times.
+try: # to prevent from crashing, wrap it in a try-statement 
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print('Lap #%s: %s (%s)' % (lapNum, totalTime, lapTime), end = '')
+        lapNum += 1
+        lastTime = time.time() # reset the last lap time
+except KeyboardInterrupt:
+    # Handle the Ctrl-C exception to keep its error message from displaying.
+    print('\nDone.')
