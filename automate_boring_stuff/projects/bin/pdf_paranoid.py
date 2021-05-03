6046abab7f3030cd0689d835472a2f10d39d2d3d
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-05-03 11:42:56 +0200

    PDF paranoia project - initial commit

---
+++ automate_boring_stuff/projects/bin/pdf_paranoid.py
@@ -0,0 +1,12 @@
+''' 
+Project from pg. 316 in Automate the Boring Stuff
+
+Using os.walk(), write a script that will:
+- go through every PDF in a folder & it's subfolders
+- encrypt each of the PDFs using a pw given in teh command line
+- save each encrypted pdf with _encrypted.pdf suffic
+
+Before deleting the original PDF file have the program:
+-
+
+'''
