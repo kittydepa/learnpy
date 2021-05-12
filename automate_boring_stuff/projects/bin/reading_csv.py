52899b0ec3653e9da9a60762e2fd8134448a49d2
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-05-12 15:08:53 +0200

    Added more comments

--- automate_boring_stuff/projects/bin/reading_csv.py
+++ automate_boring_stuff/projects/bin/reading_csv.py
@@ -4,9 +4,11 @@ From page 322 in 'Automate the Boring Stuff...'
 
 import csv
 
-exampleFile = open('example.csv')
-exampleReader = csv.reader(exampleFile)
+exampleFile = open('example.csv') # First open the csv file/bring it into the scope
+exampleReader = csv.reader(exampleFile) # Make it an object, using csv.reader
 
+
+# Now fo a for loop, that will bring the line/row number, and its contents
 for row in exampleReader:
     print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
 
