fc4156aaa1b42252651708e95b27cd6231327b1b
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-05-12 15:06:04 +0200

    For loop for reading and printing csv files

---
+++ automate_boring_stuff/projects/bin/reading_csv.py
@@ -0,0 +1,11 @@
+'''
+From page 322 in 'Automate the Boring Stuff...'
+'''
+
+import csv
+
+exampleFile = open('example.csv')
+exampleReader = csv.reader(exampleFile)
+
+for row in exampleReader:
+    print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
