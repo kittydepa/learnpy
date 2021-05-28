cc9aca3fc7e33ca54c60e8314b2c5ccca5e69573
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-05-28 14:10:44 +0200

    Ch 14. Project Instructions

--- automate_boring_stuff/projects/bin/json_weatherdata.py
+++ automate_boring_stuff/projects/bin/json_weatherdata.py
@@ -1,6 +1,28 @@
-''' Project @ end of Ch. 14 of Automate the Boring Stuff
+''' 
+Project @ end of Ch. 14 of Automate the Boring Stuff
 Fetching the current weather data with weather API/ JSON data
+
+This program will do the following:
+- Read the requested location from the command line
+- Download JSON weather data from OpenWeatherMap.org
+- Convert the string of JSON data to a Python data structure
+- Print the weather for today and the next two days
+
+There, the code will need to do the following:
+- Join strings in sys.argv to get the location
+- Call requests.get() to download the weather
+- Call json.loads() to convert the JSON data to a Python data structure
+- Print the weather forecast
+
  '''
 
 
- 
+ # Step 1: Get Location from the Comman Line Argument
+
+
+
+ # Step 2: Download the JSON Data
+
+
+
+ # Step 3: Load JSON Data and Print Weather
