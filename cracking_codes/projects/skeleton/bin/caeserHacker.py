fa88e3b85c9303a05ed6ea1c68c482ca2e43c471
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-15 19:03:26 +0200

    Hacking Caeser Ciphers without the key

---
+++ cracking_codes/projects/skeleton/bin/caeserHacker.py
@@ -0,0 +1,33 @@
+# Caeser Cipher Hacker
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+## Cracking the Caeser Cipher by *brute force*
+
+message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM' # example cipher text
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+# Loop through every possible key:
+for key in range(len(SYMBOLS)):
+    # It is important to set translated to the blank string so that the previous iteration's value for translated is cleared:
+    translated = ''
+
+    # The rest of the this program is the same as the Caeser Cipher one
+    # Loop through each symbol in message:
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symbolIndex = SYMBOLS.find(symbol)
+            translatedIndex = symbolIndex - key
+
+            # Handle the wraparound:
+            if translatedIndex < 0:
+                translatedIndex = translatedIndex + len(SYMBOLS)
+            
+            # Append the decrypted symbol:
+            translated = translated + SYMBOLS[translatedIndex]
+
+        else:
+            # Append the symbol without encrypting/decrypting:
+            translated = translated + symbol
+    
+    # Display every possible decryption:
+    print('Key #%s: %s' % (key, translated))
