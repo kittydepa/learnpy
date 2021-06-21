a70220a974e8bcfcd8219b99778070bb7219b649
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-21 22:48:26 +0200

    Find current mouse coordinates - and updates automatically constantly

--- automate_boring_stuff/projects/bin/mouseNow.py
+++ automate_boring_stuff/projects/bin/mouseNow.py
@@ -4,6 +4,7 @@ From ch. 18 in No Starch Press book 'Automate the Boring Stuff with Python"
 --------------------------------------------------------------------------------------------
 
 Writing a program that will constantly display the coordinate position of the mouse, as you move it around.
+Thie program can be used to figure out which mouse coordinates you need for GUI automation scripts
 
 The code should do the following:
 - Call the position() function to obtain the current cooridnates
@@ -24,7 +25,12 @@ try:
     while True:
         # Get and print mouse coordinates
         x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: '+ str(y).rjust(4)
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: '+ str(y).rjust(4)   # .rjust()will right-justify the strings, so they take up the same amount of space (e.g. if they have 1 or 3 digits)
+        print(positionStr, end = '')                                      # 'end', to prevent the default newline character from being added to the end of the printed line.
+        print('\b' * len(positionStr), end = '', flush = True)            # To erase ext, use the \b backspace escape chacater, to erase a character at the end of the current line 
+                                                                          # flush = T, calls the print \b backspace characters, otherwise the screen might not update the text as desired.
+
+        # Since the everything within the while loop repeats so quickly, the user won't notice when the cooridnates are updated/change when they move the most
 
 except KeyboardInterrupt:
     print('\nDone.')
