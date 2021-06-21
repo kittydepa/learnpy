c98718a978073b3c6eb3c9140fbc88417e2e9846
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-21 16:05:58 +0200

    Moving most relative to its current position

--- automate_boring_stuff/projects/bin/movingMouse.py
+++ automate_boring_stuff/projects/bin/movingMouse.py
@@ -12,3 +12,11 @@ import pyautogui
 #     pyautogui.moveTo(100, 200, duration = 0.25)
 
 # Can also use .moveRel, which will move the cursor 'relative' to the latest position of the cursor
+for i in range(10):
+    pyautogui.moveRel(100, 0, duration = 0.25)
+    pyautogui.moveRel(0, 100, duration = 0.25)
+    pyautogui.moveRel(-100, 0, duration = 0.25)
+    pyautogui.moveRel(0, -100, duration = 0.25)
+
+## How to get the current position of the mouse:
+# pyautogui.position()
