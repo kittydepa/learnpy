12ecb9b058a1c6f7b72c4ce64b9d4c44452faacb
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-22 20:29:45 +0200

    Just a comment update

--- automate_boring_stuff/projects/bin/mouseNow.py
+++ automate_boring_stuff/projects/bin/mouseNow.py
@@ -27,7 +27,7 @@ try:
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: '+ str(y).rjust(4)   # .rjust()will right-justify the strings, so they take up the same amount of space (e.g. if they have 1 or 3 digits)
         print(positionStr, end = '')                                      # 'end', to prevent the default newline character from being added to the end of the printed line.
-        print('\b' * len(positionStr), end = '', flush = True)            # To erase ext, use the \b backspace escape chacater, to erase a character at the end of the current line 
+        print('\b' * len(positionStr), end = '', flush = True)            # To erase the previous position corrdinates, use the \b backspace escape chacater, to erase a character at the end of the current line 
                                                                           # flush = T, calls the print \b backspace characters, otherwise the screen might not update the text as desired.
 
         # Since the everything within the while loop repeats so quickly, the user won't notice when the cooridnates are updated/change when they move the most
