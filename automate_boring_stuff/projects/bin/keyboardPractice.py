456d49dedfb1c94916eb80bd7deb3f8cb9076955
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-06-28 16:30:15 +0200

    Hotkey function

--- automate_boring_stuff/projects/bin/keyboardPractice.py
+++ automate_boring_stuff/projects/bin/keyboardPractice.py
@@ -1,6 +1,25 @@
-# To run this program successfully, make sure that you have a text file open in correct location your screen (the click coordinates!)
+# # To run this program successfully, make sure that you have a text file open in correct location your screen (the click coordinates!)
 
-import pyautogui
-pyautogui.click(200, 200); pyautogui.typewrite('Hey there friend. \n - Kitty', 0.25)    # (string, duraction between each key stroke)
+# import pyautogui
+# pyautogui.click(200, 200); pyautogui.typewrite('Hey there friend. \n - Kitty', 0.25)    # (string, duraction between each key stroke)
 
-# Put both commands on the same line, but separate with a semicolon to ensure that the program runs them separately
+# # Put both commands on the same line, but separate with a semicolon to ensure that the program runs them separately
+
+
+
+
+
+# Practice with HotKey combination/function
+
+import pyautogui, time
+
+def commentAfterDelay():
+    pyautogui.click(300, 300)
+    pyautogui.typewrite('In IDLE, Alt-3 comments out a line.')
+    time.sleep(2)
+    pyautogui.hotkey('alt', '3')
+
+commentAfterDelay()
+
+
+## Didn't really work for me, but w.e I get the point 
