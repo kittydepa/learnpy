99186ec43ba9bbb56a805ed92e56dafe5f11c323
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-16 10:58:12 +0200

    Simple countdown program ch 15 - initial commit

---
+++ automate_boring_stuff/projects/bin/countdown.py
@@ -0,0 +1,27 @@
+'''
+Guided project: Simple countdown program.
+Ch 15 in 'Automate the Boring Stuff with Python' No Starch Press
+
+Writing a simple countdown program that will play an alarm sound at the end of the countdown
+
+
+At a high level, the program will:
+    - Count down from 60
+    - Play the sound file (alarm.wav) when the countdown reaches 0
+
+The code will then need to:
+    - Pause for 1 second in between displaying each number in the coundown, by calling time.sleep()
+    - Call subprocess.Popen() to open the sound file with the default application
+'''
+
+## Step 1: Count down
+
+
+
+
+## Step 2: Play the sound file
+
+
+
+
+##
