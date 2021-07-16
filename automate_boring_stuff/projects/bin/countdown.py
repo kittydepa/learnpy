62f1dcf1ce4cd4095cb2d81e81b9cbe7f2b246d3
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-16 11:42:09 +0200

    Success

--- automate_boring_stuff/projects/bin/countdown.py
+++ automate_boring_stuff/projects/bin/countdown.py
@@ -12,16 +12,29 @@ At a high level, the program will:
 The code will then need to:
     - Pause for 1 second in between displaying each number in the coundown, by calling time.sleep()
     - Call subprocess.Popen() to open the sound file with the default application
+
+----------------------------------------------------------------------------------------------------------------
 '''
 
+#! python3
+#! countdown.py - A simple countdown script.
+
+import time, subprocess
+
 ## Step 1: Count down
+timeLeft = 10                     # This will hold the numer of seconds left in the countdown, starting with 60 sec
+while timeLeft > 0:
+    print('Time left in countdown = ', timeLeft)
+    print(end = '')
+    time.sleep(1)
+    timeLeft = timeLeft - 1
 
 
 
+## Step 2: Play the sound file at the end of the countdown
+subprocess.Popen(['start', 'alarm.wav'], shell = True)
 
-## Step 2: Play the sound file
-
-
-
-
-##
+'''
+Alternatively, you could also use Popen() to open/create a pop-up window,
+with a text file, that says something like 'Break time is over!'
+'''
