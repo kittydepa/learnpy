ee6fea90196602c662421d3e0333e27957d3beaa
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-17 11:37:00 +0200

    Practice with error messages

---
+++ automate_boring_stuff/projects/bin/errorExample.py
@@ -0,0 +1,15 @@
+def spam():
+    bacon()
+
+def bacon():
+    raise Exception('This is the error message, you fool.')
+
+spam()
+
+
+
+'''
+Ch 10 - Debugging
+Getting the Traceback as a String Practice p 217 
+from 'Automate the Boring Stuff with Python' No Starch Press
+'''
