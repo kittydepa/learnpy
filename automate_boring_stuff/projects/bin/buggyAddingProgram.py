5f022c8b965c393f06226bb7d71483e91663a622
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-19 11:57:52 +0200

    Comment

--- automate_boring_stuff/projects/bin/buggyAddingProgram.py
+++ automate_boring_stuff/projects/bin/buggyAddingProgram.py
@@ -1,6 +1,8 @@
 '''
 Ch 10 - Debugging a Number Adding Program using a Debugger
 From 'Automate the Boring Stuff with Python' No Starch Press
+
+Practice opening this in the Python IDLE with the Debugger
 '''
 
 print('Enter the first number to add: ')
