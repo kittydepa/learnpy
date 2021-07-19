c40f66677caee9a6a034b921ab79f77d60ba9f43
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-19 11:56:59 +0200

    Debugging a number adding program using a debugger

---
+++ automate_boring_stuff/projects/bin/buggyAddingProgram.py
@@ -0,0 +1,12 @@
+'''
+Ch 10 - Debugging a Number Adding Program using a Debugger
+From 'Automate the Boring Stuff with Python' No Starch Press
+'''
+
+print('Enter the first number to add: ')
+first = input()
+print('Enter the second number to add: ')
+second = input()
+print('Enter the third number to add: ')
+third = input()
+print('The sum is ' + first + second + third)
