d279683bb4ab85cfe4d90809085ecbe6637a5b0d
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-19 13:10:24 +0200

    Logging for debugging

--- automate_boring_stuff/projects/bin/debugCoinToss.py
+++ automate_boring_stuff/projects/bin/debugCoinToss.py
@@ -3,18 +3,33 @@ Ch 10 - Debugging from 'Automate the Boring Stuff with Python' No Starch Press
 Practice Project: Debugging Coin Toss Game
 Try to find and fix the bugs so that the program will work correctly.
 '''
-
+import logging
 import random
+
+logging.basicConfig(level = logging.DEBUG, format = ' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of the program.')
+
+# ---------------------------------------------------
+
 guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails: ')
     guess = input('> ')
+    logging.debug(f'Try 1. Participant guess is: {guess}.')
+
 toss = random.randint(0, 1) # 0 is tails, 1 in heads
+logging.debug(f'PRE-randomisation. The toss is: {toss}')
+logging.debug('0 is tails, and 1 is heads')
+
 if toss == guess:
     print('You got it!')
+    logging.debug(f'POST-randomisation. The toss is: {toss}')
+
 else:
     print('Nope! Guess again!')
-    guesss = input('> ')
+    guess = input('> ')
+    logging.debug(f'Try 2. Participant guess is: {guess}.')
+    logging.debug(f'POST-randomisation. The toss is: {toss}')
     if toss == guess:
         print('You got it!')
     else:
