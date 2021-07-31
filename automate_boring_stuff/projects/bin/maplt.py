d9bab656a536e3cdc7a7a79087b6fcb3a99bf05a
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-31 10:40:40 +0200

    Comments - instructions

--- automate_boring_stuff/projects/bin/maplt.py
+++ automate_boring_stuff/projects/bin/maplt.py
@@ -1,4 +1,17 @@
 '''
-Practicing web scraping with the webbrowser module.
-From Ch 11 in 'Automate the Boring Stuff with Python' - No Starch Press
+Practicing web scraping with the webbrowser module
+From Ch 11 in 'Automate the Boring Stuff with Python' (p. 234) - No Starch Press
+
+
+This program will:
+    - get a street address from the command line agrugments or the clipboard
+    - open the web browser to the Google Maps page for the address
+So the code will:
+    - read the command line arguments from sys.argv
+    - read the clipboard contents
+    - call the webbrowser.open() function to open the webbrowser
+-----------------------------------------------------------------------------------------
 '''
+
+import webbrowser
+webbrowser.open('http://inventwithpython.com/')
