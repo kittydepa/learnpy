623aeac2f71ddca7785d27819858d379886ff5f3
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-03 22:21:11 +0200

    Step 1 - get the command line arugments and quest the search page

--- automate_boring_stuff/projects/bin/lucky.py
+++ automate_boring_stuff/projects/bin/lucky.py
@@ -12,15 +12,23 @@ The code will do the following:
     - Find the links to each search result
     - Call the 'webbrowser.open()' function to open the web browser
 '''
-#! python3
-# lucky.py - Open several Google search results weeee
+
+
+
+import requests, sys, webbrowser, bs4
+# requests - to downloaded the search result page. BeautifulSoup to find the search result links in the HTML. webbrowser to open those links in browser tabs.
+
 
 
 ## Step 1: Get the command line arguments and request the search page
+print('Googling...') # Display text while downloading the Google page
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
 
 
 
 ## Step 2: Find all the results
+
 
 
 
