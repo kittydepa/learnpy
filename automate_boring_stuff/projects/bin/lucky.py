896a31e4c18c2143c06c919d5730f4ea6832f6e6
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-03 22:14:41 +0200

    Ch 11 Project - I'm Feeling Lucky Google Search - initial commit

---
+++ automate_boring_stuff/projects/bin/lucky.py
@@ -0,0 +1,27 @@
+'''
+Project "I'm Feeling Luck Google Search" from 'Automate the Boring Stuff with Python' No Starch Press
+
+This program will:
+    - Get search keywords from the command line arugments
+    - Retrieve the search results page
+    - Open a browser tab for each result
+
+The code will do the following:
+    - Read the command line arguments from 'sys.argv'
+    - Fetch the search result page with the 'requests' module
+    - Find the links to each search result
+    - Call the 'webbrowser.open()' function to open the web browser
+'''
+#! python3
+# lucky.py - Open several Google search results weeee
+
+
+## Step 1: Get the command line arguments and request the search page
+
+
+
+## Step 2: Find all the results
+
+
+
+## Step 3: Open web browsers for each result
