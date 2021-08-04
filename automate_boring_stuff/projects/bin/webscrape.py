0b79fe0eb7a2b97295e14f3c4b4e4068a21bf1c1
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-04 14:00:54 +0200

    Finding elemends by ID

--- automate_boring_stuff/projects/bin/webscrape.py
+++ automate_boring_stuff/projects/bin/webscrape.py
@@ -4,9 +4,22 @@ From Real Python https://realpython.com/beautiful-soup-web-scraper-python
 '''
 
 import requests
+from bs4 import BeautifulSoup
+
+
 
 URL = "https://realpython.github.io/fake-jobs/" # a static web page
 page = requests.get(URL)
-page.status_code == requests.codes.ok # to make sure it worked 
+# page.status_code == requests.codes.ok # to make sure it worked 
+# print(page.text)
 
-print(page.text)
+
+
+# Finding Elements by ID
+soup = BeautifulSoup(page.content, "html.parser") # Need to pass .content instead of .text to avoid problems with character encoding. The 2nd argument ensures you use the right parser for HTML
+results = soup.find(id = "ResultsContainer")
+print(results.prettify())
+
+
+
+# Finding Elements by ID
