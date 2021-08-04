b3b04a9014f7083908120b8d2097fcbeab57063c
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-04 22:10:39 +0200

    Add task description

--- meow_1/projects/skeleton/bin/meow_task.py
+++ meow_1/projects/skeleton/bin/meow_task.py
@@ -5,6 +5,13 @@ Additional resources:
 
 Notes for the task:
     - use module 'argparse' (https://docs.python.org/3/library/argparse.html)
+
+Basically the program needs to use the argparse module to create a command line tool where:
+    - it can convert between UUIDs and "device identifiers" (in either order)
+    - print the result to the terminal for the user to copy/use
+    - have separate flags or subcommands for the 2 required functions
+    - include help text that briefly describes how to use the tool
+
 '''
 
 import argparse
