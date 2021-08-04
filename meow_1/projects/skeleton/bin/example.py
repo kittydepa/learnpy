cda4c3a618f19f880873f26f2e1fc72b656a94e4
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-04 19:42:13 +0200

    Combining positional and optional argparse arguments

--- meow_1/projects/skeleton/bin/example.py
+++ meow_1/projects/skeleton/bin/example.py
@@ -61,8 +61,20 @@ Notes for the task:
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help = "increase output verbosity", action = "store_true")
+# parser.add_argument("-v", "--verbose", help = "increase output verbosity", action = "store_true")
 
+# args = parser.parse_args()
+# if args.verbose:
+#     print("verbosity turned on")
+
+parser.add_argument("square", type = int,
+                    help = "display a square of a given number")
+parser.add_argument("-v", "--verbose", action = "store_true",
+                    help = "increase output verbosity")
 args = parser.parse_args()
+answer = args.square ** 2
+
 if args.verbose:
-    print("verbosity turned on")
+    print("the square of {} equals {}".format(args.square, answer))
+else:
+    print(answer)
