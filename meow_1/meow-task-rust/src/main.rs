642c78ce04cc7cab91a56c64a9ba3519f3a249a4
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-07 13:10:43 +0200

    Rust UUID crate practice

--- meow_1/meow-task-rust/src/main.rs
+++ meow_1/meow-task-rust/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+// Crate UUID: https://docs.rs/uuid/0.8.2/uuid/
+
+
+
+use uuid::Uuid;
+fn main() -> Result<(), uuid::Error> {
+    let my_uuid =
+        Uuid::parse_str("936DA01F9ABD4d9d80C702AF85C822A8")?;
+    println!("{}", my_uuid.to_urn());
+    Ok(())
 }
