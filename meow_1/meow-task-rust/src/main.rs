2938401d60a030980141203cbdb1fd5f55de854c
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-08-08 12:43:13 +0200

    Rust Command line arguments Tutorial

--- meow_1/meow-task-rust/src/main.rs
+++ meow_1/meow-task-rust/src/main.rs
@@ -8,15 +8,29 @@
 
 
 
-use uuid::Uuid;
-fn main() -> Result<(), uuid::Error> {
-    let my_uuid =
-        Uuid::parse_str("936DA01F9ABD4d9d80C702AF85C822A8")?;
-    println!("{}", my_uuid.to_urn());
-    Ok(())
-}
-
-// impl Uuid {
-//     pub fn parse_str(input: &str) -> Result<Uuid, Error>
-    
+// // Practice 1
+// use uuid::Uuid;
+// fn main() -> Result<(), uuid::Error> {
+//     let my_uuid =
+//         Uuid::parse_str("936DA01F9ABD4d9d80C702AF85C822A8")?;
+//     println!("{}", my_uuid.to_urn());
+//     Ok(())
 // }
+
+// // impl Uuid {
+//     pub fn parse_str(input: &str) -> Result<Uuid, Error>
+// }
+
+
+// Practice 2
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1]; // Since the file name, which user does not need to write, takes up args[0]
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+}
