37d2e19dde850e2dbb2b7e3c7f0d62f0fc72e9f8
katherinedepa <katherinedepa@penguin.lxd>
2021-08-12 20:16:31 +0200

    Just an example

--- meow_1/meow-task-rust/src/main.rs
+++ meow_1/meow-task-rust/src/main.rs
@@ -1,5 +1,23 @@
 // Will use package 'clap' instead: https://github.com/clap-rs/clap
 
-fn main() {
 
+// Below is an example from StackOverflow: https://stackoverflow.com/questions/26478558/working-with-uuid-in-rust
+use uuid::Uuid; 
+
+fn show_uuid(uuid: &Uuid) {
+    println!("bytes: {:?}", uuid.as_bytes());
+    println!("simple: {}", uuid.to_simple());
+    println!("hyphenated: {}", uuid.to_hyphenated());
+    println!("urn: {}", uuid.to_urn());
 }
+
+fn main() {
+    // // Generate a new UUID
+    // let uuid = Uuid::new_v4();
+    // show_uuid(&uuid);
+
+    // Parse an existing UUID
+    let uuid = Uuid::parse_str("95022733-f013-301a-0ada-abc18f151006").unwrap();
+    show_uuid(&uuid);
+}
+
