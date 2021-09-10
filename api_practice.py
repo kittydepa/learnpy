455a8b243160fbc6cca3d490240c15c137f9f547
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-09-10 20:20:09 +0200

    No. of astros in space API - success

--- api_practice.py
+++ api_practice.py
@@ -1,6 +1,11 @@
 '''
 Practicing using APIs in Python - Dataquest.io blog tutorial https://www.dataquest.io/blog/python-api-tutorial
 Using the Open Notify API for the International Space Stations (ISS)
-
-
 '''
+
+import requests, json
+
+## The first endpoint example: Using the API to see how many astronauts are currently in space (not that it takes no input)
+# The tutorials uses 'https', but this causes an error...
+response = requests.get("http://api.open-notify.org/astros.json")
+print(response.status_code)
