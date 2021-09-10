73e5b8cba4bc9b67a763cad56d6f5eeeea0aa52e
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-09-10 19:52:15 +0200

    Initial commit - API practice

---
+++ api_practice.py
@@ -0,0 +1,5 @@
+'''
+Practicing using APIs in Python - Dataquest.io blog tutorial https://www.dataquest.io/blog/python-api-tutorial
+Using the Open Notify API for the International Space Stations (ISS)
+
+'''
